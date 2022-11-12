561001a337d9cafbe036bb0466f697ff19867d8f
Marcos <marcos29diciembre@gmail.com>
2022-11-12 18:33:08 +0100

    NASS ability implementation

---
+++ proyecto/Assets/Scripts/Character/Combat/Habilities/NASS/PisotónDeLaMuerte.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PisotónDeLaMuerte : Abilities
+{
+    public void Awake()
+    {
+        Role = "Damage";
+    }
+    public override void Effect(Character Figther)
+    {
+        foreach(Hexagon h in this.GetComponent<Character>().getActualBlock().neighbours)
+        {
+            if(h && h.getOccupant() && h.getOccupant().getSide() != this.GetComponent<Character>().getSide())
+            {
+                int damage = (this.GetComponent<Character>().getDamage()/3 <= h.getOccupant().getDefense()) ? 1 : this.GetComponent<Character>().getDamage()/6 - h.getOccupant().getDefense();
+                h.getOccupant().setHealth(h.getOccupant().getHealth() - damage);
+            }               
+        }
+    }
+}
