ae1c1722b83f046bc349234fe4b79d2ebdcb76d7
Marcos <marcos29diciembre@gmail.com>
2022-11-18 19:54:09 +0100

    Background and Icons

--- proyecto/Assets/Scripts/Character/Combat/Abilities/Abilities.cs
+++ proyecto/Assets/Scripts/Character/Combat/Abilities/Abilities.cs
@@ -8,6 +8,7 @@ public abstract class Abilities: MonoBehaviour
     public string Role;
     public string Name;
     public string description;
-    public Image icon;
+    public Sprite icon;
+    public Sprite ActiveIcon;
     public abstract void Effect(Character Figther);
 }
