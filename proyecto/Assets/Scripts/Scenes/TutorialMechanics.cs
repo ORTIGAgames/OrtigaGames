5e951f328076b8060ea6c9c86b52f9a9ad188aba
ORTIGAgames <jesusculebras.scf@gmail.com>
2022-11-20 19:39:13 +0100

    Inicio del tutorial de mecanicas

---
+++ proyecto/Assets/Scripts/Scenes/TutorialMechanics.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialMechanics : MonoBehaviour
+{
+    string[] tutorial = { 
+        "First of all, you need to know how to move around the level, so click on Declan, your ally, and see what happens.",
+        "Each ally has a movement and an action on each of their turns, the green hexagons represent empty spaces where they can move to, yellow ones represent allies and the red ones are for enemies. Click on an empty space to move Declan.",
+        "When all the allies end their turn the enemy starts their turn and moves on their own, so you have to think in advantage of what they would do.",
+        "Now let's attack that enemy, click on Declan and then on the enemy, green hexagons will change into blue ones to show the range of attack of your unit. Click on one of the blue spaces.",
+        "See on the top, a combat menu appears, with the stats of the ally and the possible actions, attack and ability. When one of your allies attack it will damage the enemy based on the enemies’ defense and your allies’ attack stat. And the ability depends on each ally but don't get into it now. Let's attack the enemy for the moment.",
+        "Combat and strategy is not an easy task but that is why this is just a simulacrum of a real plausible scenery. The tutorial ends here, but find my button and I come back as you require. Now end this level and kill that enemy."
+    };
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
