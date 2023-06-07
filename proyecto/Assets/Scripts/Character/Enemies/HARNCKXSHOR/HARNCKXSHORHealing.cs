a10028b3810cf227fb3de1045556e7130fc16d3c
Yibus13 <jesusculebras.scf@gmail.com>
2023-06-07 20:19:58 +0200

    Ahora si nivel final completo

--- proyecto/Assets/Scripts/Character/Enemies/HARNCKXSHOR/HARNCKXSHORHealing.cs
+++ proyecto/Assets/Scripts/Character/Enemies/HARNCKXSHOR/HARNCKXSHORHealing.cs
@@ -10,25 +10,28 @@ public class HARNCKXSHORHealing : Abilities
     {
 
         minions = GameObject.Find("Manager").GetComponent<ManagerHARNCKXSHOR>().minions;
-        foreach(Enemy m in minions.ToArray())
-        {
-            foreach(Hexagon h in this.GetComponent<Enemy>().getActualBlock().neighbours){
-                
-                    foreach (Hexagon e in h.neighbours)
+        
+        foreach(Hexagon h in this.GetComponent<Enemy>().getActualBlock().neighbours){
+
+            foreach (Hexagon e in h.neighbours)
+            {
+                foreach (Enemy m in minions.ToArray())
+                {
+                    if (e == m.GetComponent<Enemy>().getActualBlock() || h == m.GetComponent<Enemy>().getActualBlock())
                     {
-                        if (e == m.GetComponent<Enemy>().getActualBlock() || h == m.GetComponent<Enemy>().getActualBlock())
-                        {
-                            this.GetComponent<Enemy>().setHealth(this.GetComponent<Enemy>().getHealth() + 5);
-                            this.GetComponent<Enemy>().game.DeleteCharacter(m);
-                            break;
-                        }
+                        this.GetComponent<Enemy>().setHealth(this.GetComponent<Enemy>().getHealth() + 5);
+                        this.GetComponent<Enemy>().game.DeleteCharacter(m);
+
                     }
+                }
             }
+                
         }
+        
     }
 
     public override void BeforeTurn()
-    {
+    {      
        
     }
 }
